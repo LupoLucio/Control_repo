87bb08262d717b7fb0ad224753025ec6f12b14e8
enrico-bassani <enricobassani01@gmail.com>
2026-03-17 10:59:36 +0100

    Ultime modifiche es.2 Ghesborro

--- es_2_Simulink/Point 1+2+3/Data.m
+++ es_2_Simulink/Point 1+2+3/Data.m
@@ -11,18 +11,33 @@ g = 9.81;   % gravity
 % Control input
 u = 0;   % no control
 
-% Initial conditions pt1
+% pt1: Simulate the system from initial conditions θ(0) = 10 deg, 
+% θdot(0) = 0 deg/s. Compare the evolutions of both the linear 
+% and non-linear systems. What can you notice?
+
+% Initial conditions
 theta0_deg_pt1 = 10;
 theta0dot_deg_pt1 = 0;
 
-% Initial conditions pt2
-theta0_deg_pt2 = 60;
-theta0dot_deg_pt2 = 0;
-
-% Radians conversion pt1
+% Radians conversion
 theta0_pt1 = deg2rad(theta0_deg_pt1);
 theta0dot_pt1 = deg2rad(theta0dot_deg_pt1);
 
-% Radians conversion pt2
+%Here the approximation is valid since the initial angle is small and 
+% the two graphs are identical.
+
+
+% pt1: Simulate the system from initial conditions θ(0) = 60 deg, 
+% θdot(0) = 0 deg/s. Compare the evolutions of both the linear 
+% and non-linear systems. What can you notice?
+
+% Initial conditions
+theta0_deg_pt2 = 60;
+theta0dot_deg_pt2 = 0;
+
+% Radians conversion
 theta0_pt2 = deg2rad(theta0_deg_pt2);
 theta0dot_pt2 = deg2rad(theta0dot_deg_pt2);
+
+%Here the approximation is no more valid since the initial angle is 
+% quite large and the two graphs are a little bit different.
--- es_2_Simulink/Point 4/Data.m
+++ es_2_Simulink/Point 4/Data.m
@@ -13,6 +13,10 @@ A = [0 1;
 
 B = [0; 1];
 
+C = [0 1];
+
+D = 0;
+
 p = [ 1i -1i;
      -1+1i -1-1i;
      -10+1i -10-1i;
@@ -23,33 +27,28 @@ for i = 1:size(p,1);
     K{i} = place(A, B, p(i,:));
 end
 
-B_new = [0; 0];
-
-C_new = [0 0];
-
-D_new = 0;
-
 for i=1:size(p,1)
     
-    sys_1= ss(A-(K{i}')*(B'),B_new,C_new,D_new);
+    Acl = A - B*K{i};
+    sys_cl = ss(Acl, B, C, D);
     x0 = [2; 0]';
-    t = 0:0.01:10;
-    [y1,t1,x1] = initial(sys_1,x0,t);
+    t = 0:0.01:20;
+    [y1,t1,x1] = initial(sys_cl,x0,t);
     
     figure; 
     subplot(2,1,1);
     plot(t1,x1); 
-    axis([0 10 -2 2]);
+    %%axis([0 10 -2 2]);
     title('x(t) with friction  initial conditions θ(0) = 2 rad, θdot(0) = 0 rad/s'); 
     grid on;
     
     x0 = [0.1; 0]';
-    t = 0:0.01:10;
-    [y1,t1,x1] = initial(sys_1,x0,t);
+    t = 0:0.01:20;
+    [y1,t1,x1] = initial(sys_cl,x0,t);
     
     subplot(2,1,2)
     plot(t1,x1); 
-    axis([0 10 -0.1 0.1]);
+    %%axis([0 10 -0.1 0.1]);
     title('x(t) with friction  initial conditions θ(0) = 0.1 rad, θdot(0) = 0 rad/s'); 
     grid on;
 
