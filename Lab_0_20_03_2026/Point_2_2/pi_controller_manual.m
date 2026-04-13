5011f0a6f501d8b8221bd72bfd31233ea9d44f6a
enrico-bassani <enricobassani01@gmail.com>
2026-04-13 23:03:59 +0200

    pulizia 3

--- Lab_0_20_03_2026/Point_2_2/pi_controller_manual.m
+++
@@ -1,8 +0,0 @@
-function [kp,ki,kd,tl,type] = pi_controller_manual(wgc_p,delta_phi_rad,delta_K,wgc)
-
-        type = 'PI';
-        kp = delta_K * cos(delta_phi_rad);
-        ki = -wgc * delta_K * sin(delta_phi_rad);
-        kd = 0;
-        tl = 1 / (wgc_p * wgc); 
-end
