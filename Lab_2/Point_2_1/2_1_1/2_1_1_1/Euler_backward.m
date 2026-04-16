21588d02cc9e375f8e20bcc1c1fe09a0cae2fa6d
LupoLucio <farinati.lucio@gmail.com>
2026-04-16 23:38:37 +0200

    implementazione backward euler discretization

--- Lab_2/Point_2_1/2_1_1/2_1_1_1/Euler_backward.m
+++ Lab_2/Point_2_1/2_1_1/2_1_1_1/Euler_backward.m
@@ -6,35 +6,24 @@ T3 = 0.050;   % 50 ms
 % Scegli quello da usare in questo momento:
 T = T2;
 
-% 3. Discretizzazione con Eulero all'indietro (metodo spazio di stato)
-% Estrazione matrici di stato
-[A, B, C, D] = ssdata(C_s);
-n = size(A, 1);
-I = eye(n);
-
-% Formule di discretizzazione Eulero all'indietro (implicito)
-% x[k] = x[k-1] + Ts*(A*x[k] + B*u[k])  =>  (I - Ts*A)*x[k] = x[k-1] + Ts*B*u[k]
-Ad = inv(I - T*A);
-Bd = T * Ad * B;
-Cd = C * Ad;
-Dd = D + T * C * Ad * B;
-
-% Creazione sistema discreto
-C_z_ss = ss(Ad, Bd, Cd, Dd, T);
-C_z = tf(C_z_ss); % Conversione in TF discreta
-
-
-
-
-
 z = tf('z', T);
 
-% Tre termini separati
+s_lap = (z-1)/ (z*T);
+
+
+C_z = kp + ki/s_lap + kd*s_lap/(1 + tl*s_lap);
+
+% Risultato da confrontare
 C_P = kp;
 C_I = ki * (T*z) / (z - 1);
 C_D = kd * (z - 1) / ((tl + T)*z - tl);
 
 % Controllore completo
-C_pid_z = C_P + C_I + C_D;
+C_pid_z = (C_P + C_I + C_D);
+
+
+
+C_z_real = minreal(C_z);
+C_pid_z_real = minreal(C_pid_z);
 
 
